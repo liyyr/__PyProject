9c061d6b8bb02408bc44d54c9fe0f193af3b6a42
Hare <liyyr.me@gmail.com>
2017-09-11 22:30:20 +0800

    查询不同数据库
    通过数据库列表，分别查询不同数据库里面的内容

--- pyMysqlTest.py
+++ pyMysqlTest.py
@@ -1,38 +1,37 @@
 import pymysql
 
-db = pymysql.connect('localhost','root','root','serverlist')
-
-cursor = db.cursor()
-
-sql = 'select * from server'
 ServerList = []
 try:
-	cursor.execute(sql)
+	db = pymysql.connect('localhost','root','root','serverlist')
+	cursor = db.cursor()
+	cursor.execute('select * from server')
 	results = cursor.fetchall()
 	for row in results:
 		host = row[1]
 		username = row[2]
 		pwd = row[3]
+		prefixed = row[4]
 		#print('host= %s, username= %s, pwd= %s' % (host, username, pwd))
-		ServerList.append([host, username, pwd])
+		ServerList.append([host, username, pwd, prefixed])
 except:
 	print("serverlist is error")
 
 try:
 	n = 0
 	while n < len(ServerList):
-		host, username, pwd = ServerList[0]
-		db2 = pymysql.connect(host, username, pwd,'serverlist')
+		host, username, pwd, prefixed = ServerList[n]
+		db2 = pymysql.connect(host, username, pwd, prefixed+'_ww_account')
 		cursor2 = db2.cursor()
-		cursor2.execute('select * from server')
+		cursor2.execute('select * from orders')
 		results2 = cursor2.fetchall()
 		for row2 in results2:
-			host2 = row2[1]
-			username2 = row2[2]
-			pwd2 = row2[3]
-			print('host= %s, username= %s, pwd= %s' % (host2, username2, pwd2))
+			Id = row2[0]
+			OrderDate = row2[1]
+			OrderPrice = row2[2]
+			Customer = row2[3]
+			print('Id= %d, OrderDate= %s, OrderPrice= %d, Customer= %s' % (Id, OrderDate, OrderPrice, Customer))
 		n += 1
-except Exception as e:
-	raise e
+except:
+	print("DBSelect is error")
 
 db.close()
